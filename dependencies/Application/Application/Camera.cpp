3df6c1121d306469a39044a442e58e1f7d59b9ba
CaptainStar10 <stefansef14@gmail.com>
2026-07-03 16:34:03 +0300

    Update Camera.cpp

--- dependencies/Application/Application/Camera.cpp
+++ dependencies/Application/Application/Camera.cpp
@@ -7,7 +7,7 @@ Camera::Camera(int width, int height, glm::vec3 position)
 	Position = position;
 }
 
-void Camera::Matrix(float FOVdeg, float nearPlane, float farPlane,GLuint shaderProgram, const char* uniform)
+void Camera::Matrix(float FOVdeg, float nearPlane, float farPlane, Shader& shader, const char* uniform)
 {
 	// Initializes matrices since otherwise they will be the null matrix
 	glm::mat4 view = glm::mat4(1.0f);
@@ -19,7 +19,7 @@ void Camera::Matrix(float FOVdeg, float nearPlane, float farPlane,GLuint shaderP
 	projection = glm::perspective(glm::radians(FOVdeg), (float)width / height, nearPlane, farPlane);
 
 	// Exports the camera matrix to the Vertex Shader
-	glUniformMatrix4fv(glGetUniformLocation(shaderProgram, uniform), 1, GL_FALSE, glm::value_ptr(projection * view));
+	glUniformMatrix4fv(glGetUniformLocation(shader.ID, uniform), 1, GL_FALSE, glm::value_ptr(projection * view));
 }
 
 
