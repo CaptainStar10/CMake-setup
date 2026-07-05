7622c27c313f078111bc4b0800558658eae4e9cd
CaptainStar10 <stefansef14@gmail.com>
2026-07-05 14:48:29 +0300

    Update Camera.h

--- dependencies/Application/Application/core/Camera.h
+++ dependencies/Application/Application/core/Camera.h
@@ -1,34 +1,44 @@
-#pragma once
-#include <glad/glad.h>
-#include <GLFW/glfw3.h>
-#include <glm/glm.hpp>
-#include <glm/gtc/matrix_transform.hpp>
-#include <glm/gtc/type_ptr.hpp>
-#include <glm/gtx/rotate_vector.hpp>
-#include <glm/gtx/vector_angle.hpp>
+#ifndef CAMERA_CLASS_H
+#define CAMERA_CLASS_H
 
-#include "shaderClass.h"
+#include<glad/glad.h>
+#include<GLFW/glfw3.h>
+#include<glm/glm.hpp>
+#include<glm/gtc/matrix_transform.hpp>
+#include<glm/gtc/type_ptr.hpp>
+#include<glm/gtx/rotate_vector.hpp>
+#include<glm/gtx/vector_angle.hpp>
 
-struct Camera
+//#include"shaderClass.h"
+
+class Camera
 {
+public:
+	// Stores the main vectors of the camera
 	glm::vec3 Position;
 	glm::vec3 Orientation = glm::vec3(0.0f, 0.0f, -1.0f);
 	glm::vec3 Up = glm::vec3(0.0f, 1.0f, 0.0f);
 
+	// Prevents the camera from jumping around when first clicking left click
 	bool firstClick = true;
 	bool IsUp = false;
 	bool InputType = 0;
 
+	// Stores the width and height of the window
 	int width;
 	int height;
 
+	// Adjust the speed of the camera and it's sensitivity when looking around
 	float speed = 0.1f;
 	float sensitivity = 100.0f;
 
+	// Camera constructor to set up initial values
 	Camera(int width, int height, glm::vec3 position);
 
-	void Matrix(float FOVdeg, float nearPlane, float farPlane, Shader& shader, const char* uniform);
+	// Updates and exports the camera matrix to the Vertex Shader
+	void Matrix(float FOVdeg, float nearPlane, float farPlane, GLuint shaderProgram, const char* uniform);
+	// Handles camera inputs
 	void Inputs(GLFWwindow* window);
-	//This is under implementation.DO NOT USE IT(is useless)
 	void NewInput(GLFWwindow* window);
 };
+#endif
