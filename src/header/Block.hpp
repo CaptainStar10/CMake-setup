4b9fc28182d0e413f77087182dd88242a1dcb8fe
CaptainStar10 <stefansef14@gmail.com>
2026-07-05 14:45:26 +0300

    Add files via upload

---
+++ src/header/Block.hpp
@@ -0,0 +1,13 @@
+#pragma once
+#include <Application/core/Application.hpp>
+#include <Application/core/VertexArrayObject.h>
+#include <Application/core/VertexBufferObject.h>
+#include <Application/core/ElementBufferObject.h>
+#include <Application/core/ShaderClass.h>
+#include <Application/core/Texture.h>
+
+class Block
+{
+	public:
+		Block() = default;
+};
